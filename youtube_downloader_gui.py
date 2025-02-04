7308e9a65b672e360ad056245c584f9801843519
MrTesi <89657356+MrTesi@users.noreply.github.com>
2025-02-04 20:58:10 -0300

    youtube_downloader_gui.py

--- youtube_downloader_gui.py
+++ youtube_downloader_gui.py
@@ -123,7 +123,7 @@ def baixar_video():
 
 # Criando a janela principal
 root = tk.Tk()
-root.title("Video Downloader")
+root.title("Download De Videos")
 root.geometry("600x600")
 root.configure(bg="#2E2E2E")
 
